07c7c83325e661cc5e637c5e1779018b2c0ee3d1
gooohjy <gooohjiayi@gmail.com>
2020-06-14 03:48:02 +0800

    Updates to page content

--- js/init.js
+++ js/init.js
@@ -174,7 +174,7 @@ var eric = function (){
 	
 	/*Typed js start*/
 	$("#typed").typed({
-		strings: ["^2100&nbsp;I Am Violeta.<br/>Designer &amp; Photographer.<br/> Living In Berlin City."],
+		strings: ["^2100&nbsp;I am Jia Yi."],
 		typeSpeed: 30,
 		backDelay: 750,
 		loop: false,
